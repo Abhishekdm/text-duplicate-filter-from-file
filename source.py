d0ac6f4ccc3ab44a6ddcf59cc278b75d77d0619a
abhishek dm <abhishekd301@gmail.com>
2019-05-26 17:56:44 +0530

    Create source.py

---
+++ source.py
@@ -0,0 +1,18 @@
+def checkLine(rline):
+    write_file = open('output.txt', 'r')
+    for line in write_file:
+        # elements passed as paramaters for lstrip will remove the matching elements from the leftside of the string
+        wline = line.lstrip('0123456789.')
+        if (wline == rline):
+            return True
+
+
+read_file = open('text.txt', 'r')
+for line in read_file:
+    if(checkLine(line.lstrip('0123456789.'))):
+        print("line already exist")
+    else:
+        wrt_file = open('output.txt', 'a')
+        wrt_file.write(line)
+        wrt_file.close()
+print("completed")
